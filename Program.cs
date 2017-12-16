f37d90c2520563414f7cb45899a667757d1f366b
Travon Billups <travonbillups@yahoo.com>
2017-12-15 20:24:33 -0600

    playing around with process.start function. Using the System.Diagnostics class header.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace autoCorrect
 {
@@ -10,6 +11,7 @@ namespace autoCorrect
     {
         static void Main(string[] args)
         {
+            Process.Start(@"c:\\");
         }
     }
 }
